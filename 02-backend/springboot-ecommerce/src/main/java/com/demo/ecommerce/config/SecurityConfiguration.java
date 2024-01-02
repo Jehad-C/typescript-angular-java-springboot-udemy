3953bfd09df34d9048ed2c20c0ba0e5bbdb99267
Jehad Cemanes <jehadcemanesece@gmail.com>
2024-01-03 00:04:11 +0800

    Add order history security

---
+++ 02-backend/springboot-ecommerce/src/main/java/com/demo/ecommerce/config/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package com.demo.ecommerce.config;
+
+import com.okta.spring.boot.oauth.Okta;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                        configurer
+                                .requestMatchers("/api/orders/**")
+                                .authenticated()
+                                .anyRequest()
+                                .permitAll())
+                .oauth2ResourceServer(oaut2 ->
+                        oaut2.jwt(Customizer.withDefaults()))
+                .cors(Customizer.withDefaults())
+                .csrf(csrf ->
+                        csrf.disable())
+                .setSharedObject(ContentNegotiationStrategy.class,
+                                 new HeaderContentNegotiationStrategy());
+
+        Okta.configureResourceServer401ResponseBody(http);
+        return http.build();
+    }
+}
--- 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -1,7 +1,7 @@
 export default {
     oidc: {
         clientId: '{CLIENT-ID}',
-        issuer: 'http://example.com',
+        issuer: 'https://example.com',
         redirectUri: 'http://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email'],
     }
